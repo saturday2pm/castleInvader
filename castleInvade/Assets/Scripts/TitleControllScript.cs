81ffe931d3aceab552c23f91c8376f0a4e4875b4
ozt88 <kl1467@gmail.com>
2016-10-03 18:22:40 +0900

    CASTLE INVADE INIT

---
+++ castleInvade/Assets/Scripts/TitleControllScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleControllScript : MonoBehaviour {
+
+    public void OnClickStartButton()
+    {
+        Debug.Log("start button clicked!");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnClickExitButton()
+    {
+        Debug.Log("exit button clicked!");
+        Application.Quit();
+    }
+}
