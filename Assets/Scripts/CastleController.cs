cf414c7bebef0b042cf9fcf94d46121690e3f2ad
YoungdaeCho <zootyducky@gmail.com>
2016-10-15 18:13:29 +0900

    CastleController에 Collision 넣고, 배경화면에 Collision넣고 callback을 InputController에 넣어두었다. TODO : InputController callback 구현

---
+++ Assets/Scripts/CastleController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class CastleController : MonoBehaviour
+{
+    public UILabel UnitCountLabel;
+    public UI2DSprite CastleSprite;
+    public UI2DSprite SelectorSprite;
+
+    static InputController InputController;
+
+
+    int id = -1;
+    public int Id { get { return id; } }
+
+    public bool Init(int _id, Vector2 _position, int _unitCount, float _size, Color _color)
+    {
+        id = _id;
+        transform.localPosition = new Vector3(_position.x, _position.y, 0);
+        UpdateCastle(_unitCount, _size, _color);
+        return true;
+    }
+
+    public void UpdateCastle(int _unitCount, float _size, Color _color)
+    {
+        CastleSprite.color = _color;
+
+        int rectSize = (int)(_size * 2.0f);
+        CastleSprite.width = rectSize;
+        CastleSprite.height = rectSize;
+        SelectorSprite.width = rectSize;
+        SelectorSprite.height = rectSize;
+
+        UnitCountLabel.text = _unitCount.ToString();
+        float labelYPos = (CastleSprite.height + UnitCountLabel.height) * 0.5f;
+        UnitCountLabel.transform.localPosition = new Vector3(0, labelYPos, 0);
+    }
+
+    public void OnClick()
+    {
+        if (!InputController)
+        {
+            var obj = GameObject.FindGameObjectWithTag("InputController");
+            if(obj)
+            {
+                InputController = obj.GetComponent<InputController>();
+                if(!InputController)
+                {
+                    Debug.Log("InputController is Missing!");
+                }
+            }
+        }
+        InputController.OnCastleClick(this);
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class InputController : MonoBehaviour {
+
+    CastleController PrevSelectCastle;
+    CastleController CurSelectCastle;
+    bool IsClickActivate = false;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void OnCastleClick(CastleController castle)
+    {
+        
+    }
+
+    public void OnBackgroundClick()
+    {
+       
+    }
+}
