0cc519aa1b92474407b58b9363eaeaa04e6ee95b
ozt88 <kl1467@gmail.com>
2016-10-22 16:55:12 +0900

    facebook login

---
+++ Assets/Scripts/LoginScene/FacebookLogin.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Facebook.Unity;
+
+public class FacebookLogin : MonoBehaviour {
+    public UIButton loginButton;
+    public UITexture profilePicture;
+
+    public void Awake()
+    {
+        FB.Init(() =>
+        {
+            loginButton.SetState(UIButtonColor.State.Normal, true);
+        });
+    }
+
+    public void OnFacebookLogin()
+    {
+        var permissions = new string[] {
+            "public_profile"
+        };
+
+        FB.LogInWithReadPermissions(
+            permissions,
+            LoginCallback);
+    }
+
+    private void LoginCallback(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            var token = AccessToken.CurrentAccessToken;
+
+            string userId = token.UserId;
+            string accessToken = token.TokenString;
+
+            Debug.Log("user id : " + userId);
+            Debug.Log("token : " + accessToken);
+
+            FB.API("/" + userId + "/picture?type=large", HttpMethod.GET, OnPicture);
+        }
+        else
+        {
+            // login error
+        }
+    }
+
+    private void OnPicture(IGraphResult result)
+    {
+        if (result.Error == null)
+        {
+            profilePicture.mainTexture = result.Texture;
+        }
+    }
+}
