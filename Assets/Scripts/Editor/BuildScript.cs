6dd579a05273c220227c86385ad784b3f93437bb
Park June Chul <pjc0247@naver.com>
2016-11-06 03:39:47 +0900

    add buildscripts

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+
+public class BuildScript
+{
+    public static void BuildAndroid()
+    {
+        Build(BuildTarget.Android);
+    }
+
+    public static void BuildOSX()
+    {
+        Build(BuildTarget.StandaloneOSXIntel64);
+    }
+
+    public static void BuildWindows()
+    {
+        Build(BuildTarget.StandaloneWindows);
+    }
+
+    public static void Build(BuildTarget target)
+    {
+        var scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
+
+        BuildPipeline.BuildPlayer(scenes,
+            Environment.GetCommandLineArgs().Last(),
+            target,
+            BuildOptions.None);
+    }
+}
